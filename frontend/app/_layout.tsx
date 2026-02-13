12c800d6b577e04488f8249cf5f8ee77fb720df4
yanneckk <yanneck.kolb@protonmail.com>
2026-02-13 18:08:04 +0100

    feat(splashscreen): 1.1.6 added a splashscreen

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,10 +1,52 @@
 import { Stack } from "expo-router";
 import { AuthProvider } from "../features/auth/authContext";
+import { useEffect, useState } from 'react';
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native';
+
 
 export default function RootLayout() {
+    const [appReady, setAppReady] = useState(false);
+
+    useEffect(() => {
+        async function prepare() {
+            try {
+                // TODO: Load await (example loadResourcesAsync()
+                await new Promise(resolve => setTimeout(resolve, 2000));
+            } catch (e) {
+                console.warn("Error while Loading:", e);
+            } finally {
+                setAppReady(true);
+            }
+        }
+
+        prepare();
+    }, []);
+
+    if (!appReady) {
+        return (
+            <View style={styles.splashContainer}>
+                <Text style={styles.logo}>Netro</Text>
+                <ActivityIndicator size="large" color="#007AFF" />
+            </View>
+        );
+    }
     return (
         <AuthProvider>
             <Stack screenOptions={{ headerShown: false }} />
         </AuthProvider>
     );
 }
+
+const styles = StyleSheet.create({
+    splashContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#fff',
+    },
+    logo: {
+        fontSize: 48,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+});
